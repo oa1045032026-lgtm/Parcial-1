00f88cdb4d969a32ff0a8160da0dc6086a6f53b0
OA1045032026-LGTM <oa1045032026@unab.edu.sv>
2026-04-09 18:27:45 -0600

    practica

---
+++ buclewhile.py
@@ -0,0 +1,18 @@
+# si tenemos una variable y necesitamos comprobar
+
+clima = "caliente"  ##  clima por defecto
+# entrada = input("¿Cómo está el clima? ")
+## print("El clima es: ", entrada)
+
+# para igualar un valor vamos a tomar ==
+numeroComparar = False
+
+
+if numeroComparar == False:
+    print("debes de trabajar para comprar una terreno para que tus nietos  constr.")
+else:
+    print("Debes cuidar tus rodillas")
+
+
+# erifica un camino si este camino no esta deduce una opcion por decto
+   
